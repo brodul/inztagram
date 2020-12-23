b00cdf332f1903732946685b22fdc4bc32a23908
Andraz Brodnik <brodul@brodul.org>
2020-12-23 15:13:08 +0100

    Fix build process

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
+// https://github.com/parcel-bundler/parcel/issues/4550 
+// postCSS in parcel can not detect NODE_ENV
+const devMode = process.env['npm_lifecycle_script'] !== 'parcel "src\index.html"'
+console.log(devMode ? "In development mode" : "In production mode")
+
 module.exports = {
-  purge: [
-    // './src/**/*.html',
-  ],
+  purge: {
+    content: ['./src/**/*.html'],
+    enabled: !devMode,
+  }
+  ,
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
